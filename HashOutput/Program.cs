be3fb66a57a3ab819c6e9c6467ad704c22303cf6
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-12 11:40:27 +0100

    Add project files.

---
+++ HashOutput/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HashOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            for (var i = rows; i > 0; i--)
+            {
+                for (var j = rows; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (var k = 0; k < (i * 2); k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
