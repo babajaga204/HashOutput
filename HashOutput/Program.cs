9654e422e24a1b53f9af075836beb6f836f2a33c
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-13 09:56:03 +0100

    Done with Exercise 2.3

--- HashOutput/Program.cs
+++ HashOutput/Program.cs
@@ -1,23 +1,83 @@
-﻿namespace HashOutput
+﻿using System.Data;
+
+namespace HashOutput
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int rows = 4;
-            for (var i = rows; i > 0; i--)
-            {
-                for (var j = rows; j > i; j--)
-                {
-                    Console.Write(" ");
-                }
+            //Exercise2_1();
+            //Exercise2_2();
+            //Exercise2_3();
+            Exercise2_9();
+        }
 
-                for (var k = 0; k < (i * 2); k++)
-                {
-                    Console.Write("#");
-                }
-                Console.WriteLine();
+        static void Exercise2_3()
+        {
+            for (var row = 0; row < 4; row++)
+            {
+                Space(row);
+                Hash(row + 1);
+                Space(6 - 2 * row);
+                Space(6 - 2 * row);
+                Hash(row + 1);
+                NewLine();
+            }
+
+            for (var row = 3; row >= 0; row--)
+            {
+                Space(row);
+                Hash(row + 1);
+                Space(6 - 2 * row);
+                Space(6 - 2 * row);
+                Hash(row + 1);
+                NewLine();
             }
         }
+
+        static void Exercise2_1()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Space(i);
+                Hash(8 - 2 * i);
+                NewLine();
+            }
+        }
+
+        static void Exercise2_2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Space(3 - i);
+                Hash(2 + 2 * i);
+                NewLine();
+            }
+            Exercise2_1();
+        }
+        static void Space()
+        {
+            Console.Write(" ");
+        }
+
+        static void Space(int count)
+        {
+            for (var i = 0; i < count; i++) Console.Write(" ");
+        }
+        static void Hash()
+        {
+            Console.Write('#');
+        }
+        static void Hash(int count)
+        {
+            for (var i = 0; i < count; i++) Hash();
+        }
+
+        static void NewLine()
+        {
+            Console.WriteLine();
+        }
+
+
     }
 }
